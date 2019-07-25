33b1f9fd9a4a2dd50343cfc1f30c094b9c8a86a7
Max Lefebvre <max.lefebvre@garvin-allen.com>
2019-07-25 15:40:36 -0300

    additional cleanup

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -7,19 +7,20 @@
  *
  */
 
+// libs
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'containers/HomePage/Loadable';
+// containers/components
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
-import GlobalStyle from '../../global-styles';
+// utils
+import GlobalStyle from 'global-styles';
 
 export default function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
